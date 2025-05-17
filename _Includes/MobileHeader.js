ffa3dd8448fe8235d208151939ffcb0f879a7653
Andrew Benson <ab@bensonfamilies.net>
2025-05-16 22:24:08 -0400

    Initial site creation.

---
+++ _Includes/MobileHeader.js
@@ -0,0 +1,15 @@
+﻿var openElements = document.getElementsByClassName("open-mobile-nav");
+
+for (var i = 0; i < openElements.length; i++) {
+    openElements[i].addEventListener('click', function () {
+        document.getElementsByTagName("body")[0].classList.add("mobile-nav-open");
+    });
+}
+
+var closeElements = document.getElementsByClassName("close-mobile-nav");
+
+for (var i = 0; i < closeElements.length; i++) {
+    closeElements[i].addEventListener('click', function () {
+        document.getElementsByTagName("body")[0].classList.remove("mobile-nav-open");
+    });
+}
---
+++ legal/scripts.js
@@ -0,0 +1,8 @@
+﻿const container = document.querySelector('.details-group');
+
+// Close all other details when one is shown
+container.addEventListener('sl-show', event => {
+	if (event.target.localName === 'sl-details') {
+		[...container.querySelectorAll('sl-details')].map(details => (details.open = event.target === details));
+	}
+});
